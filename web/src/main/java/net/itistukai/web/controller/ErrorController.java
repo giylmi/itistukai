0e3f775bde164dc2fe297237fcac4567bcecb62f
giylmi <adel.giylmi@gmail.com>
2015-03-15 23:40:46 +0400

    error pages

--- web/src/main/java/net/itistukai/web/controller/ErrorController.java
+++ web/src/main/java/net/itistukai/web/controller/ErrorController.java
@@ -1,18 +1,31 @@
 package net.itistukai.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by ainurminibaev on 14.03.15.
  */
+@ControllerAdvice
 @Controller
 public class ErrorController {
+
+    private Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
     @RequestMapping(value = "/forbidden", method = RequestMethod.GET)
     public String renderError() {
         //TODO generate forbidden page
         return "redirect:/";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestMapping("404")
+    public String notFound(Exception ex){
+        logger.error("caught exception: ", ex);
+        return "site/notFound";
     }
 }
---
+++ web/src/main/webapp/WEB-INF/templates/site/notFound.ftl
@@ -0,0 +1,13 @@
+<#include "mainTemplate.ftl"/>
+<#macro m_body>
+<header>
+    <div class="container">
+        <div class="intro-text">
+            <!--<div class="intro-lead-in">проект</div>-->
+            <div class="intro-heading">Вы заблудились в лесу. Советую вернуться</div>
+            <a href="/" class="page-scroll btn btn-xl">Вернуться</a>
+        </div>
+    </div>
+</header>
+</#macro>
+<@m_page title="Страница не найдена"/>
