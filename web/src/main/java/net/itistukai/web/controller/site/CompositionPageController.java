a8a6b9cbacaf553b4923e31037beda9a4aef8327
giylmi <adel.giylmi@gmail.com>
2015-03-20 22:13:47 +0300

    fix getting composition by name (cherry picked from commit dd9c658) (cherry picked from commit f86cbf3)

--- web/src/main/java/net/itistukai/web/controller/site/CompositionPageController.java
+++ web/src/main/java/net/itistukai/web/controller/site/CompositionPageController.java
@@ -31,7 +31,7 @@ public class CompositionPageController {
                                   @RequestParam(required = false, defaultValue = "date_desc") SortType sort,
                                   @RequestParam(required = false, defaultValue = "grid") String viewType,
                                   @RequestParam(required = false) Long partId){
-        Composition composition = compositionService.getAll().get(0);
+        Composition composition = compositionService.getTheOne();
         Part part = null;
         for (Part compositionPart: composition.getParts())
             if (compositionPart.getId().equals(partId)) {
