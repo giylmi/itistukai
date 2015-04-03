f6c5236016fd1c4c4106702937622e50df4ea08c
Adel Gilmullin <adel.giylmi@gmail.com>
2015-04-03 22:50:18 +0300

    hideRepeat (cherry picked from commit 735167f)

--- web/src/main/java/net/itistukai/web/service/impl/VideosServiceImpl.java
+++ web/src/main/java/net/itistukai/web/service/impl/VideosServiceImpl.java
@@ -8,7 +8,6 @@ import net.itistukai.web.service.VideosService;
 import net.itistukai.web.sort.SortType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
